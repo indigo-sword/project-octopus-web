a0908c00dc213e2966138e5fecef16507f752249
joao <Joao.Henares@btgpactual.com>
2024-01-20 15:54:29 -0500

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <title>Project Octopus</title>
+      </header>
+      <h1>Project Octopus</h1>
+    </div>
+  );
+}
+
+export default App;
