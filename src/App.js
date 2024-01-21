f8a332812c1dcd91a2963e42a4940fc117aad74e
joao <Joao.Henares@btgpactual.com>
2024-01-21 13:19:36 -0500

    changed from hash router into browser router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 import React from "react";
-import { Route, Routes, HashRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import About from "./pages/about";
 import Home from "./pages/home";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
