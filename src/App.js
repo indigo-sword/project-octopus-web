550020251e01169142a8612de939b895b73fa7a3
joao <Joao.Henares@btgpactual.com>
2024-01-21 01:14:51 -0500

    start

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header>
-        <title>Project Octopus</title>
-      </header>
       <h1>Project Octopus</h1>
     </div>
   );
